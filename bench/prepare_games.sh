f8eec0b064451f5b6203e49bdf2b44c38a94ffaa
Peter Bortas <bortas@gmail.com>
2019-07-16 23:06:10 +0100

    Separated games.lst into game sets that correspond to other benchmark efforts for easy comparisons

--- bench/prepare_games.sh
+++ bench/prepare_games.sh
@@ -4,6 +4,8 @@ export MAME_EXEC=/mametest/stored-mames/pie-mame0211-gcc8-1b969a8acb/mame
 export ROMPATH=/mametest/roms
 export PATH=~/ia-rcade:$PATH
 
+sort gameset-*.lst | uniq > games.lst
+
 cat games.lst | while read game; do
     # NOTE: ia-cade does not return any error codes, so it needs manual monitoring
     ia-rcade -rompath $ROMPATH -noexecmame $game
