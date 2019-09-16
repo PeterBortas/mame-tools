0cc2a8ad39e992b6d0620e4f5a8190b2318dd77a
Peter Bortas <bortas@gmail.com>
2019-09-16 23:49:00 +0100

    Dummy Coverity model to stop Scan complaining about incomplete setup

---
+++ coverity/coverity_model.c
@@ -0,0 +1,10 @@
+/* Coverity dummy model
+ *
+ * This files models no actual behaviours right now, it is only
+ * uploaded to stop Coverity scan telling us that the project setup
+ * isn't complete.
+ */
+
+void covdummy_zino(int dummy) {
+    __coverity_panic__();
+}
