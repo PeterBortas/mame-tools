8856cdadb9e6d094efc2b07b4476fb605f486ecb
Peter Bortas <bortas@gmail.com>
2019-09-22 01:25:12 +0200

    Clean up and modularize the benchmark a bit

---
+++ bench/pi_resumable_benchmark.sh
@@ -0,0 +1,210 @@
+#!/bin/bash
+
+# Benchmarks whatever needs benchmarking continues to do so even if there is a reboot.
+
+# Version of mame to benchmark (can be omitted to continue an aborted run)
+VER=$1
+
+BENCHDIR=$(dirname $0)
+MAMEBASE="/mametest"
+CC=gcc8
+ONLYONCE=1  # should games be skipped if a benchmark already exists?
+
+LOCKFILE="/run/lock/`basename $0`"
+LOCKFD=17
+
+source ${BENCHDIR}/../functions.sh
+
+# Avoid running multiple instances of script.
+_prepare_locking
+exlock_now || exit 0  # Locking cleanup is handled by a trap
+
+# Install cronjob if missing
+if crontab -l | grep pi_resumable_benchmark >/dev/null 2>&1; then
+    : # cronjob already installed
+else
+    echo "Installing cronjob..."
+    (crontab -l ; \
+     echo "*/5 * * * * cd $HOME/mame-tools/bench && ./pi_resumable_benchmark.sh") | crontab -
+fi
+
+# Get Mame version to benchmark either from argv[1] or runstate/CURRENT_VERSION
+# side effect; sets VER and TAG
+set_mame_version $VER
+
+# Fail before starting to create dirs and cruft if any throttling flags have been triggered
+reboot_if_throttled
+
+if [ $(getconf LONG_BIT) -eq 64 ]; then
+    EXE64=64
+fi
+MAME=$(ls -d $MAMEBASE/arch/$(uname -m)-$(getconf LONG_BIT)/stored-mames/mame${TAG}-$CC-*/mame$EXE64)
+RUNID=$(basename $(dirname $MAME))-$(date '+%Y-%m-%dT%H:%M:%S')
+LOGFILE=logs/$RUNID.log
+STATEDIR=runstate/$RUNID
+mkdir -p logs
+mkdir -p $STATEDIR
+# Redirect everything to the log file
+exec >$LOGFILE 2>&1
+set -x
+
+ROMPATH=$(get_mame_romdir $VER)
+
+export DISPLAY=:0
+
+# SDL defaults to OpenGL renderer if it exists, but it's not
+# accellerated on the Pi, force EGL. (Not tested, requires mame to
+# use the render-target code)
+export SDL_RENDER_DRIVER=opengles2
+
+if $BENCHDIR/sdl2test-zino; then
+    : # echo "NOTE: Renderer tested OK"
+else
+    echo "FATAL: Renderer not available"
+    exit 1
+fi
+
+# Hardcode mame.ini, verified to be picked up even if -cfg_directory is used
+# Note: All the artwork options are probably useless in ~0.212+
+cat > ~/.mame/mame.ini <<EOF
+#
+# CORE ARTWORK OPTIONS
+#
+artwork_crop  0
+use_backdrops 0
+use_overlays  0
+use_bezels    0
+use_cpanels   0
+use_marquees  0
+#
+# CORE SOUND OPTIONS
+#
+samplerate    22050
+#
+# OSD ACCELERATED VIDEO OPTIONS
+#
+filter        0
+EOF
+
+# X doesn't start if not connected to a screen at startup, can be
+# worked around by hardcoding setup in /boot/config.txt
+if ! xset q >/dev/null; then
+    echo "FATAL: Unable to connect to the X server"
+    exit 1
+fi
+
+# Make sure we have booted the RPi in the 640x480 benchmark resolution
+# TODO: Why does this make so much differance? Scaling should be free.
+# NOTE: RPi4 does not seem to support extracting the enumerated HDMI ports by name, so grep
+if [ "$(sudo vcgencmd get_config int | grep hdmi_group)" != "hdmi_group:0=2" ]; then
+    echo "FATAL: hdmi_group != 2"
+    exit 1
+fi
+if [ "$(sudo vcgencmd get_config int | grep hdmi_mod)" != "hdmi_mode:0=4" ]; then
+    echo "FATAL: hdmi_group != 2"
+    exit 1
+fi
+
+# Avoid variability due to SD card swapping or zram overhead
+if [ $(/sbin/swapon | wc -l) -gt 0 ]; then
+    echo "FATAL: swap is enabled"
+    exit 1
+fi
+
+turn_off_screensavers
+
+# Make sure we don't start the test accidentally while something else is running
+wait_for_load
+
+function get_temp {
+    vcgencmd measure_temp | sed 's/temp=\(.*\)\..*/\1/'
+}
+
+# Throttling sets in at 80C, so leave at least a 15C envelope to work in
+function wait_for_cooldown {
+    init_cool=1
+    while [ $(get_temp) -gt 64 ]; do
+	if [ $init_cool -eq 1 ]; then
+	    echo "Waiting for CPU to cool down before next run..."
+	    init_cool=0
+	fi
+	sleep 1
+	echo -ne "$(get_temp)C  \r"
+    done
+    echo
+    if [ $init_cool -eq 0 ]; then
+	echo " OK"
+    fi
+}
+
+# FIXME: Before the below message can be removed and actual
+# benchmarks can be done the following must be automatically
+# checked:
+# [/] Make sure SDL is using hardware accel for scaling (SDL_RENDER_DRIVER _should_ do that)
+
+# echo "This file does not contain a valid publishable benchmark" >> $LOGFILE
+
+# TODO: Nice things to have
+# [ ] Log any sdram and GPU overclock
+# [ ] -str saves the final frame in the snap dir. Do something with it
+
+echo "ARM overclock status: $(get_freq_arm) kHz" >> $LOGFILE
+echo "GPU overclock status: $(get_freq_gpu) kHz" >> $LOGFILE
+echo "RAM overclock status: $(get_freq_sdram) kHz" >> $LOGFILE
+
+GAMEARGS="-rompath $ROMPATH -cfg_directory $STATEDIR/cfg -nvram_directory $STATEDIR/nvram -snapshot_directory $STATEDIR/snap -diff_directory $STATEDIR/diff"
+
+# Don't allow benchmarks to run for more than 10 x test time, aka 15min
+TIMEOUT_WAIT=900
+TIMEOUT="timeout --kill-after=5 $TIMEOUT_WAIT"
+
+# Some games need initial setup to not be stuck forever on some setup
+# screen. These are created manually by starting the game with
+# make_initial_state.sh
+echo "Installing initial state in test environment..."
+for x in initial_state/*; do
+    echo $x...
+    (cd $x && tar cf - * | (cd ../../$STATEDIR && tar xvf -))
+done
+
+mkdir -p runstate/gameresults
+
+while read -r game; do
+    case $game in
+    *#*)
+	echo "Note: Skipping $game"
+	continue
+    esac
+    gamelog=$(get_gamelog_name $game $CC $ONLYONCE)
+    if [ $? -eq -1 ]; then
+	echo "NOTE: Skipping $game, $gamelog already exists"
+	continue
+    fi
+    echo "Starting $game at $(date)"
+    $MAME -listfull           $GAMEARGS $game >> $gamelog 2>&1
+    wait_for_cooldown
+    echo "Before run: $(get_temp) $(vcgencmd get_throttled)" >> $gamelog
+    echo "Running real emulation benchmark" >> $gamelog
+    $TIMEOUT $MAME -str 90 -nothrottle $GAMEARGS $game >> $gamelog 2>&1
+    if [ $? -eq 124 ]; then
+	echo "Timed out after ${TIMEOUT_WAIT}s" >> $gamelog
+    fi
+    wait_for_cooldown
+    echo "Running built in benchmark" >> $gamelog
+    $TIMEOUT $MAME -bench 90           $GAMEARGS $game >> $gamelog 2>&1
+    echo "After run: $(get_temp) $(vcgencmd get_throttled)" >> $gamelog
+    if [ $? -eq 124 ]; then
+	echo "Timed out after ${TIMEOUT_WAIT}s" >> $gamelog
+    fi
+    echo "Completed $game at $(date)"
+    reboot_if_throttled
+done < games.lst
+
+echo "Completed run of $VER-$(get_system_idname)-$CC at $(date), removing from automatic queue"
+rm runstate/CURRENT_VERSION
+
+# Pull a new version from the queue and let cron take care of starting it
+queue_next_version
+
+# Uncomment if screensaver should be reactivated
+#xscreensaver-command -restart
---
+++ functions.sh
@@ -0,0 +1,225 @@
+# PRIVATE
+_lock()             { flock -$1 $LOCKFD; }
+_no_more_locking()  { _lock u; _lock xn && rm -f $LOCKFILE; }
+_prepare_locking()  { eval "exec $LOCKFD>\"$LOCKFILE\""; trap _no_more_locking EXIT; }
+
+# PUBLIC
+exlock_now()        { _lock xn; }  # obtain an exclusive lock immediately or fail
+unlock()            { _lock u; }   # drop a lock
+
+# RPi specific throttle check
+function was_throttled {
+    local throttled=$(vcgencmd get_throttled)
+    # Do not report throttling on pure undervoltage events
+    if [ throttled != "throttled=0x0" -a throttled != "throttled=0x50000" ]; then
+	true
+    else
+	false
+    fi
+}
+
+# RPi specific
+function reboot_if_throttled {
+    if was_throttled; then
+	echo "FATAL: Pi has been throttled, will reboot at $(date)" >> runstate/reboot.log
+	./parse_throttle.py >> runstate/reboot.log
+	sudo reboot
+	exit 1 # reboot does not block
+    fi
+}
+
+# RPi specific
+function get_freq_arm {
+    vcgencmd get_config arm_freq | awk -F= '{print $2}'
+}
+
+# RPi specific
+function get_freq_gpu {
+    vcgencmd get_config gpu_freq | awk -F= '{print $2}'
+}
+
+# RPi specific (Note: not applicable on RPi4)
+function get_freq_sdrom {
+    vcgencmd get_config sdram_freq | awk -F= '{print $2}'
+}
+
+# Get Mame version to benchmark either from argv[1] or runstate/CURRENT_VERSION
+# side effect; sets VER and TAG
+function set_mame_version {
+    if [ -z $1 ]; then
+	if [ ! -f runstate/CURRENT_VERSION ]; then
+	    echo "Usage: $0 <version>"
+	    echo "Example: $0 0.212"
+	    exit 1
+	else
+	    VER=$(cat runstate/CURRENT_VERSION)
+	fi
+    else
+	if [ -f runstate/CURRENT_VERSION ]; then
+	    echo "FATAL: There is already an automatic benchmark of $(cat runstate/CURRENT_VERSION) in progress!"
+	    exit 1
+	else
+	    VER=$1
+	    echo $VER > runstate/CURRENT_VERSION
+	fi
+    fi
+    TAG=$(echo $VER | sed 's/\.//')
+}
+
+function get_num_sockets {
+    local numsock=$(cat /proc/cpuinfo  | grep "physical id" | sort | uniq | wc -l)
+    if [ $numsock -eq 0 ]; then
+	echo 1
+    else
+	echo $numsock
+    fi
+}
+
+function get_cpu_type {
+    cat /proc/cpuinfo  | grep "model name" | head -1 | awk -F: '{print $2}' | awk '$1=$1'
+}
+
+function get_mem_gigs {
+    # system never gets the last few megs, so lets integer this and add 1
+    echo $(( $(free -m | grep Mem: | awk '{print $2}') / 1024 +1 ))
+}
+
+function get_system_type {
+    if [ -f /sys/firmware/devicetree/base/model ]; then
+	case $(tr -d '\0' </sys/firmware/devicetree/base/model) in
+	"Raspberry Pi 4"*)
+	    echo "Raspberry Pi 4"
+	    return ;;
+	*)
+	    echo "FATAL: Unknown system type"
+	    exit 1
+	esac
+    fi
+
+    # dmidecode needs sudo, so hardcoding here we go!
+    case $(uname -n) in
+    "crux")
+	echo "ProLiant DL160 G6"
+	return ;;
+    n16[0-9][0-9])
+	echo "DL980 G7"
+	return ;;
+    n[0-9]*)
+	echo "SL230s G8"
+	return ;;
+    esac
+}
+
+function get_system_shortname {
+    if [ "$(get_system_type)" = "Raspberry Pi 4" ]; then
+	echo "rpi4"
+	return
+    fi
+
+    local cpu=$(get_cpu_type | sed 's/(R)//g')
+    case $cpu in
+    "Intel Xeon CPU"*)
+	local id=$(echo $cpu | awk '{print tolower($4)}')
+	echo "xeon_$id"
+	return ;;
+    esac
+}
+
+# Something that identifies the hardware and it's configuration,
+# suitable as substring in a filename.
+function get_system_idname {
+    local extra=""
+    case $(get_system_shortname) in
+    "rpi4")
+	# TODO: Handle frequencies generically
+	case $(get_freq_arm) in
+	    1500) ;; # default frequency
+	    1750) extra="-1.75" ;;
+	    2000) extra="-2.0" ;;
+	    *)
+		echo "FATAL: unhandled CPU frequency $(get_freq_arm)" 1>&2
+		exit 1
+	esac
+	case $(get_freq_gpu) in
+	    500) ;; # default frequency
+	    600) extra="${extra}-G600" ;;
+	    *)
+		echo "FATAL: unhandled GPU frequency $(get_freq_gpu)" 1>&2
+		exit 1
+	esac
+	echo $(get_system_shortname)$extra
+	return
+	;;
+    *)
+	echo $(get_system_shortname)
+	return ;;
+    esac
+}
+
+function get_mame_romdir {
+    tmp=$MAMEBASE/roms/internetarchive
+    if [ -e $MAMEBASE/roms/0.212 ]; then
+	tmp=$MAMEBASE/roms/0.212
+    fi
+    if [ -e $MAMEBASE/roms/$1 ]; then
+	tmp=$MAMEBASE/roms/$1
+    fi
+    echo $tmp
+}
+
+function turn_off_screensavers {
+    # This might disable the the default X11 screen saver:
+    xset s noblank
+    xset s off
+    xset -dpms
+    # But this is probably the only thing useful on a "modern" desktop:
+    xscreensaver-command -exit
+}
+
+function wait_for_load {
+    init_load=1
+    while (( $(echo "$(awk '{print $1}' /proc/loadavg) > 0.5" |bc -l) )); do
+	if [ $init_load -eq 1 ]; then
+	    echo -n "Waiting for load to go down before starting test.."
+	    init_load=0
+	fi
+	sleep 2
+	echo -n "."
+    done
+    if [ $init_load -eq 0 ]; then
+	echo " OK"
+    fi
+}
+
+function queue_next_version {
+    while read -r x; do
+	case $x in
+	0.[0-9]*)
+	    echo "Queueing up $x"
+	    echo $x > runstate/CURRENT_VERSION
+	    sed -i 's/^'$x'$/d' runstate/queue
+	    exit 0
+	    ;;
+	esac
+    done < runstate/queue
+}
+
+function get_gamelog_name {
+    local game=$1
+    local CC=$2
+    local once=$3
+    local base="runstate/gameresults/$game-$(get_system_idname)-$CC.result"
+    i=1
+    local log=$base.$i
+    if [ -f $log ]; then
+	if [ $once -eq 1 ]; then
+	    return -1
+	fi
+    fi
+    while [ -f $log ]; do
+	i=$(( i+1 ))
+	log=$base.$i
+    done
+    echo $log
+    return 0
+}
