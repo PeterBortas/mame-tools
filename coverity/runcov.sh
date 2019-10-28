c4a38c57498058ff9666931df7dbf7f2896d5124
Peter Bortas <bortas@gmail.com>
2019-10-28 15:11:55 +0100

    Archive old log at the start of each build

--- coverity/runcov.sh
+++ coverity/runcov.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 cd $(dirname $0)
+mkdir -p oldlogs
+
+# FIXME: This needs a lock, the log will be moved under any running
+# build, and they don't keep an fd
+if [ -f build.log ]; then
+    mv -v build.log oldlogs/build.log.$(date --iso-8601=seconds)
+fi
 
 echo "Build started $(date)" >> build.log
 ./covbuild.sh                >> build.log 2>&1
