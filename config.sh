f1664958c293cea6e3e12481b41293dcf5025cc0
Peter Bortas <bortas@gmail.com>
2019-11-02 22:44:12 +0100

    configuration

---
+++ config.sh
@@ -0,0 +1,13 @@
+# Compilation setup
+GCCVER=8
+COMP_CC=gcc-$GCCVER
+COMP_CXX=g++-$GCCVER
+
+# Generic benchmark setup
+GEN_BENCH_CC=gcc$GCCVER
+GEN_ONLYONCE=0
+
+# Raspberry Pi benchmark setup
+PI_BENCH_CC=gcc$GCCVER
+PI_ONLYONCE=0
+PI_TESTREAL=1
